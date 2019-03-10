14652a797ad153aed78d649992d82d2270277429
Alex Sherman <asherman1024@gmail.com>
2019-03-09 20:29:19 -0600

    Missing files

---
+++ BenefactAPI/Controllers/StorageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BenefactAPI.Controllers
+{
+    [Route("api/storage/")]
+    public class StorageController : Controller
+    {
+        [HttpGet("get/{guid}")]
+        public async Task<ActionResult> Get(Guid guid)
+        {
+            if (guid == new Guid())
+                throw new HTTPError("Invalid guid");
+            return new ContentResult();
+        }
+    }
+}
---
+++ BenefactAPI/Controllers/StorageInterface.cs
@@ -0,0 +1,18 @@
+﻿using Replicate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BenefactAPI.Controllers
+{
+    [ReplicateType(AutoMethods = AutoAdd.AllPublic)]
+    public class StorageInterface
+    {
+        IServiceProvider Services;
+        public StorageInterface(IServiceProvider services)
+        {
+            Services = services;
+        }
+    }
+}
