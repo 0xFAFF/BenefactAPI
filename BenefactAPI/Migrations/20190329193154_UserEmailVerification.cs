f21a75791e11ebf0a50df0441eee498baa6134b2
Alex Sherman <asherman1024@gmail.com>
2019-04-02 14:55:42 -0500

    Missed migrations from last commit

---
+++ BenefactAPI/Migrations/20190329193154_UserEmailVerification.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BenefactAPI.Migrations
+{
+    public partial class UserEmailVerification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "email_verified",
+                table: "users",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "nonce",
+                table: "users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email_verified",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "nonce",
+                table: "users");
+        }
+    }
+}
